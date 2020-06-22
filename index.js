a6437437f874e92e2c931289ee831d86806eade6
Pascal Weiland <weiland@users.noreply.github.com>
2020-06-22 12:36:12 +0200

    Adjust Prettier settings to v2.0.0

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = {
-  printWidth: 120,
-  semi: true,
+  printWidth: 120,         // prettier recommends 80 (old Unix-Style)
+  tabWidth: 2,             // default
+  useTabs: false,          // default
+  semi: true,              // default
   singleQuote: true,
-  bracketSpacing: false,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-  tabWidth: 2,
-  useTabs: false,
+  quoteProps: 'as-needed', // default
+  trailingComma: 'es5',    // default since 2.0.0
+  bracketSpacing: true,    // default
+  arrowParens: 'always',   // default since 2.0.0
+  endOfLine: 'lf',         // default since 2.0.0
 };
