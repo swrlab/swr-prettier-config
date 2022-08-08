a54289a6658b4d36ac6201d5b6ccb45061213cac
Daniel Freytag <daniel@frytg.com>
2022-08-08 12:32:58 +0200

    fix: update config

--- index.js
+++ index.js
@@ -8,5 +8,14 @@ module.exports = {
 	trailingComma: 'es5', // default since 2.0.0
 	bracketSpacing: true, // default
 	arrowParens: 'always', // default since 2.0.0
-	endOfLine: 'lf', // default since 2.0.0
+	endOfLine: 'lf', // default since 2.0.0,
+	overrides: [
+		{
+			files: ['*.md'],
+			options: {
+				useTabs: false,
+				tabWidth: 2,
+			}
+		}
+	]
 }
